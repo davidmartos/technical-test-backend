5a666a8ea9c39b5c8d43cd913edaa1e5e51c3a80
David Martos Grande <davidmartos92@gmail.com>
2024-08-01 16:29:46 +0200

    Removed unused tag

--- src/main/java/com/playtomic/tests/wallet/WalletApplication.java
+++ src/main/java/com/playtomic/tests/wallet/WalletApplication.java
@@ -3,13 +3,11 @@ package com.playtomic.tests.wallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableCaching
 @EnableRetry
-@EnableJpaAuditing
 public class WalletApplication {
 
     public static void main(String[] args) {
